f12c586ade7f653296cf3806ac2eadd3140e5772
Andrew Cue <andrewcue@outlook.com>
2026-09-19 13:17:33 -0400

    updated app.py and added search.html to display search results

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "hello"
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/search", methods="POST")
+def display_results():
+    #the code added here will be used to get the data and send it to the frontend
+    return render_template("search.html")
