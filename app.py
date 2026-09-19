c31e6e25b5ab07d110660f2d6d0fe54d626c220e
Andrew Cue <andrewcue@outlook.com>
2026-09-19 14:02:51 -0400

    form submission

--- app.py
+++ app.py
@@ -7,22 +7,15 @@ app = Flask(__name__)
 @app.route("/search", methods=["POST"])
 def display_results():
     #the code added here will be used to get the data and send it to the frontend
-    print(request.args.get('from'))
-    print(request.args.get('to'))
+    print(request.form.get('from_city'))
+    print(request.form.get('to_city'))
     return render_template("search.html")
 
-
-
-
-
-@app.route("/search", methods=["POST"])
-def display_results():
-    #the code added here will be used to get the data and send it to the frontend
-    return render_template("search.html")
 
 @app.route("/")
 def home():
       return render_template("index.html")
+
 
 
 if __name__ == "__main__":
